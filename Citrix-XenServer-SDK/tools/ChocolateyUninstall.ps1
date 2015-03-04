68b560d98b7f4f13c6fd1a229ab4cd06673de75a
Iain Brighton <iainbrighton@hotmail.com>
2015-03-04 10:24:06 +0000

    Added Citrix-XenServer-SDK

---
+++ Citrix-XenServer-SDK/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+﻿## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for MSI installations
+try {
+    Get-WmiObject -Class Win32_Product | Where { $PSItem.Name -eq 'Citrix XenServerPSSnapin' } | ForEach-Object {
+	    Uninstall-ChocolateyPackage -PackageName 'Citrix-XenServer-SDK' -FileType 'MSI' -SilentArgs "$_.IdentifyingNumber /qn /norestart"; 
+    }
+}
+catch {
+    throw $_.Exception;
+}
