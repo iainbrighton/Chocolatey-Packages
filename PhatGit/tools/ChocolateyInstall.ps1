0bb931c6decf183485ef7b77ef885d982b4509e7
Iain Brighton <iainbrighton@hotmail.com>
2015-03-04 21:19:17 +0000

    Added PhatGit

---
+++ PhatGit/tools/ChocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for Zip-based Powershell module installations
+try {
+    $moduleInstallPath = '{0}\WindowsPowershell\Modules' -f [System.Environment]::GetFolderPath('Personal');
+    if ($env:chocolateyPackageParameters -like '*AllUsers*') {
+        $moduleInstallPath = '{0}\WindowsPowershell\Modules' -f $env:ProgramFiles;
+    }
+    Install-ChocolateyZipPackage 'PhatGit' 'https://github.com/iainbrighton/PhatGit/releases/download/v1.0.3.18/PhatGit-v1.0.3.18.zip' "$moduleInstallPath";
+}
+catch {
+    throw $_.Exception;
+}
---
+++ PhatGit/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,11 @@
+﻿## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for Zip-based Powershell module installations
+try {
+    $moduleInstallPath = '{0}\WindowsPowershell\Modules' -f [System.Environment]::GetFolderPath('Personal');
+    if ($env:PackageParameters -like '*AllUsers') {
+        $moduleInstallPath = '{0}\WindowsPowershell\Modules' -f $env:ProgramFiles;
+    }
+    Remove-Item -Path "$moduleInstallPath\PhatGit" -Recurse -Force;
+}
+catch {
+    throw $_.Exception;
+}
