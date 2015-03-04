8eede115b7f0395d70330433c7f09132bb736916
Iain Brighton <iainbrighton@hotmail.com>
2015-03-04 12:03:17 +0000

    Added PicPick.Portable package

---
+++ PicPick.Portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for Zip-based portable installations
+try {
+    $packageToolsPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
+    $packageRootPath = Split-Path -Path $packageToolsPath -Parent;
+    $packageContentPath = Join-Path -Path $packageRootPath -ChildPath 'Content';
+    [Ref] $null = New-Item -Path $packageContentPath -ItemType Directory;
+    foreach ($exe in (@('picpick.exe','cloudexplorer.exe'))) {
+        $exePath = Join-Path -Path $packageContentPath -ChildPath "$exe.gui";
+        [Ref] $null = New-Item -Path $exePath -ItemType File -Force;
+    }
+    Install-ChocolateyZipPackage 'PicPick.Portable' 'http://www.nteworks.com/latestdownload/picpick_portable.zip' "$packageContentPath";
+}
+catch {
+    throw $_.Exception;
+};
