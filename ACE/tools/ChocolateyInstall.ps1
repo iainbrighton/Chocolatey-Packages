619ee7794826bd7a529d9db9ffb256897a7f668f
Iain Brighton <iainbrighton@hotmail.com>
2015-03-04 11:27:33 +0000

    Added ACE package

---
+++ ACE/tools/ChocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for bundled EXE/MSI installations
+$packageToolsPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
+$packagePath = Split-Path -Path $packageToolsPath -Parent;
+$bundleFilePath = Join-Path -Path $packagePath -ChildPath 'ACE.MSI';
+Install-ChocolateyInstallPackage -PackageName 'ACE' -FileType 'MSI' -SilentArgs '/qn /norestart' -File $bundleFilePath;
---
+++ ACE/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+﻿## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for MSI installations
+try {
+    Get-WmiObject -Class Win32_Product | Where { $_.Name -eq 'App-V 5 Configuration Editor' } | ForEach-Object {
+	    Uninstall-ChocolateyPackage -PackageName 'ACE' -FileType 'MSI' -SilentArgs "$($_.IdentifyingNumber) /qn /norestart"; 
+    }
+}
+catch {
+    throw $_.Exception;
+}
