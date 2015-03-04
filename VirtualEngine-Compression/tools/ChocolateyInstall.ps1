5192e9555df628b1b6c3c2f72c3cfbb4e9f58763
Iain Brighton <iainbrighton@hotmail.com>
2015-03-04 21:17:51 +0000

    Added VirtualEngine-Compression

---
+++ VirtualEngine-Compression/tools/ChocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for Zip-based Powershell module installations
+try {
+    $moduleInstallPath = '{0}\WindowsPowershell\Modules' -f [System.Environment]::GetFolderPath('Personal');
+    if ($env:chocolateyPackageParameters -like '*AllUsers*') {
+        $moduleInstallPath = '{0}\WindowsPowershell\Modules' -f $env:ProgramFiles;
+    }
+    Install-ChocolateyZipPackage 'VirtualEngine-Compression' 'https://github.com/VirtualEngine/Compression/releases/download/v1.1.0.18/VirtualEngine.Compression-v1.1.0.18.zip' "$moduleInstallPath";
+}
+catch {
+    throw $_.Exception;
+}
