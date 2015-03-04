02e86d6d75d94bd9e3c2dba5cc1adbe245d7b3fa
Iain Brighton <iainbrighton@hotmail.com>
2015-03-04 11:34:51 +0000

    Added VET package

---
+++ VET/tools/ChocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for bundled EXE/MSI installations
+$packageToolsPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
+$packagePath = Split-Path -Path $packageToolsPath -Parent;
+$bundleFilePath = Join-Path -Path $packagePath -ChildPath 'VET.MSI';
+Install-ChocolateyInstallPackage -PackageName 'VET' -FileType 'MSI' -SilentArgs '/qn /norestart' -File $bundleFilePath;
---
+++ VET/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+﻿## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for MSI installations
+try {
+    Get-WmiObject -Class Win32_Product | Where { $_.Name -eq 'Virtual Engine Toolkit' } | ForEach-Object {
+	    Uninstall-ChocolateyPackage -PackageName 'VET' -FileType 'MSI' -SilentArgs "$($_.IdentifyingNumber) /qn /norestart"; 
+    }
+}
+catch {
+    throw $_.Exception;
+}
