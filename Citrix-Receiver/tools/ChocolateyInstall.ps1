4ce105a88aa58e64331e3d99ceb6d4ae7b0821a9
Iain Brighton <iainbrighton@hotmail.com>
2015-03-04 11:56:59 +0000

    Added Citrix-Receiver package

---
+++ Citrix-Receiver/tools/ChocolateyInstall.ps1
@@ -0,0 +1,2 @@
+﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for EXE/MSI installations
+Install-ChocolateyPackage -PackageName 'Citrix-Receiver' -FileType 'EXE' -SilentArgs '/noreboot /silent' -Url 'http://downloadplugins.citrix.com/Windows/CitrixReceiver.exe';
