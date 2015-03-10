6a462aebd337b7dce48f2ae321e3856ea8d58d2e
Iain Brighton <iainbrighton@hotmail.com>
2015-03-10 10:27:33 +0000

    Added Citrix-XenServer-SDK summary and valid install/uninstall exit codes

--- Citrix-XenServer-SDK/tools/ChocolateyInstall.ps1
+++ Citrix-XenServer-SDK/tools/ChocolateyInstall.ps1
@@ -2,4 +2,4 @@
 $packageToolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition;
 Install-ChocolateyZipPackage -PackageName 'Citrix-XenServer-SDK' -Url 'http://downloadns.citrix.com.edgesuite.net/akdlm/7289/XenServer-6.2.0-SDK.zip' -UnzipLocation $packageToolsPath;
 $packageFilePath = Join-Path $packageToolsPath -ChildPath 'XenServer-SDK\XenServerPSSnapIn\XenServerPSSnapIn-6.2.0-1.msi';
-Install-ChocolateyInstallPackage -PackageName 'Citrix-XenServer-SDK' -FileType 'MSI' -SilentArgs '/qn /norestart' -File $packageFilePath;
+Install-ChocolateyInstallPackage -PackageName 'Citrix-XenServer-SDK' -FileType 'MSI' -SilentArgs '/qn /norestart' -File $packageFilePath -ValidExitCodes @(0,3010);
