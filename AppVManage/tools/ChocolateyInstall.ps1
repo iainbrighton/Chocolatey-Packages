df81df8974f19a482829a7318c4787adca455f53
Iain Brighton <iainbrighton@hotmail.com>
2015-03-10 10:20:50 +0000

    Added AppvManage summary and valid install/exit codes

--- AppVManage/tools/ChocolateyInstall.ps1
+++ AppVManage/tools/ChocolateyInstall.ps1
@@ -3,4 +3,4 @@ $packageToolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition;
 Install-ChocolateyZipPackage -PackageName 'appvmanage' -Url 'http://www.tmurgent.com/appv/Images/Tools/AppV_Manage/V3/Setup_AppV_Manage_3.11.0.0.zip' -UnzipLocation "$packageToolsPath";
 ## Install Msi
 $setupAppVManagePath = Join-Path -Path $packageToolsPath -ChildPath 'Setup_AppVManage.msi';
-Install-ChocolateyInstallPackage -PackageName 'appvmanage' -FileType 'MSI' -SilentArgs '/qn /norestart' -File "$setupAppVManagePath";
+Install-ChocolateyInstallPackage -PackageName 'appvmanage' -FileType 'MSI' -SilentArgs '/qn /norestart' -File "$setupAppVManagePath" -ValidExitCodes @(0,3010);
--- AppVManage/tools/ChocolateyUninstall.ps1
+++ AppVManage/tools/ChocolateyUninstall.ps1
@@ -1,7 +1,7 @@
 ﻿## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for MSI installations
 try {
     Get-WmiObject -Class Win32_Product | Where { $_.Name -eq 'AppV_Manage' } | ForEach-Object {
-	    Uninstall-ChocolateyPackage -PackageName 'appvmanage' -FileType 'MSI' -SilentArgs "$($_.IdentifyingNumber) /qn /norestart"; 
+	    Uninstall-ChocolateyPackage -PackageName 'appvmanage' -FileType 'MSI' -SilentArgs "$($_.IdentifyingNumber) /qn /norestart" -ValidExitCodes @(0,3010); 
     }
 }
 catch {
