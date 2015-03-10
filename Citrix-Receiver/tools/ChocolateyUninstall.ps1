24daf5ab82cfb8664eaaed4abedfe66ee4e12d59
Iain Brighton <iainbrighton@hotmail.com>
2015-03-10 10:35:31 +0000

    Added Citrix-Receiver summary and valid install/uninstall exit codes

--- Citrix-Receiver/tools/ChocolateyUninstall.ps1
+++ Citrix-Receiver/tools/ChocolateyUninstall.ps1
@@ -1,2 +1,2 @@
 ﻿## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for EXE installations
-Uninstall-ChocolateyPackage -PackageName 'Citrix-Receiver' -FileType 'EXE' -SilentArgs '/silent /uninstall /cleanup' -File "$env:ALLUSERSPROFILE\Citrix\Citrix Receiver\TrolleyExpress.exe"; 
+Uninstall-ChocolateyPackage -PackageName 'Citrix-Receiver' -FileType 'EXE' -SilentArgs '/silent /uninstall /cleanup' -File "$env:ALLUSERSPROFILE\Citrix\Citrix Receiver\TrolleyExpress.exe" -ValidExitCodes @(0); 
