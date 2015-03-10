65a1b2012049d2bf09fab8bc1e4a0ca09cf3d23f
Iain Brighton <iainbrighton@hotmail.com>
2015-03-10 10:35:04 +0000

    Fixed package name in ChocolateyUninstall.ps1 Added NitroReader.Install summary and valid install/uninstall exit codes

--- NitroReader.Install/tools/ChocolateyUninstall.ps1
+++ NitroReader.Install/tools/ChocolateyUninstall.ps1
@@ -1,10 +1,9 @@
 ﻿## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for MSI installations
 try {
     Get-WmiObject -Class Win32_Product | Where { $_.Name -eq 'Nitro Reader 3' } | ForEach-Object {
-	    Uninstall-ChocolateyPackage -PackageName 'Citrix-XenServer-SDK' -FileType 'MSI' -SilentArgs "$($_.IdentifyingNumber) /qn /norestart"; 
+	    Uninstall-ChocolateyPackage -PackageName 'nitroreader.install' -FileType 'MSI' -SilentArgs "$($_.IdentifyingNumber) /qn /norestart" -ValidExitCodes @(0,3010); 
     }
 }
 catch {
     throw $_.Exception;
 }
-
