76f02b17ae77c40d75470a64e30a07e191fe61fd
Iain Brighton <iainbrighton@hotmail.com>
2015-03-10 10:04:08 +0000

    Added PoshTools packages

---
+++ PoshTools-VisualStudio2012/tools/ChocolateyInstall.ps1
@@ -0,0 +1,2 @@
+﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for Visual Studio VSIX deployment
+Install-ChocolateyVsixPackage -PackageName 'PoshTools-VisualStudio2012' -Url 'https://github.com/adamdriscoll/poshtools/releases/download/v1.0.5/PowerShellTools_2012.vsix';
