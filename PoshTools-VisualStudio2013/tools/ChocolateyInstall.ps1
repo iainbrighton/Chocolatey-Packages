3676fd6ba259a9a6cc49ec0a1061d582e0aa6fe4
Iain Brighton <iainbrighton@hotmail.com>
2015-07-06 16:44:09 +0100

    Altered PoshTools-VisualStudio2013 download location from VS Gallery to Github Altered PoshTools-VisualStudio2015 download location from VS Gallery to Github

--- PoshTools-VisualStudio2013/tools/ChocolateyInstall.ps1
+++ PoshTools-VisualStudio2013/tools/ChocolateyInstall.ps1
@@ -1,2 +1,2 @@
 ﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for Visual Studio VSIX deployment
-Install-ChocolateyVsixPackage -PackageName 'PoshTools-VisualStudio2013' -Url 'https://visualstudiogallery.msdn.microsoft.com/f65f845b-9430-4f72-a182-ae2a7b8999d7/file/166502/3/PowerShellTools.12.0.vsix';
+Install-ChocolateyVsixPackage -PackageName 'PoshTools-VisualStudio2013' -Url 'https://github.com/adamdriscoll/poshtools/releases/download/v3.0.158/PowerShellTools.12.0.vsix';
