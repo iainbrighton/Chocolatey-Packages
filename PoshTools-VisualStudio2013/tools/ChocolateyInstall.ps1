dd0db6a72e4c28e07d7d67184ff59af0cfa116c7
Iain Brighton <iainbrighton@hotmail.com>
2015-07-10 09:37:58 +0100

    Updated PoshTools-VisualStudio201x to version 3.0.163

--- PoshTools-VisualStudio2013/tools/ChocolateyInstall.ps1
+++ PoshTools-VisualStudio2013/tools/ChocolateyInstall.ps1
@@ -1,2 +1,2 @@
 ﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for Visual Studio VSIX deployment
-Install-ChocolateyVsixPackage -PackageName 'PoshTools-VisualStudio2013' -Url 'https://github.com/adamdriscoll/poshtools/releases/download/v3.0.158/PowerShellTools.12.0.vsix';
+Install-ChocolateyVsixPackage -PackageName 'PoshTools-VisualStudio2013' -Url 'https://github.com/adamdriscoll/poshtools/releases/download/v3.0.163/PowerShellTools.12.0.vsix';
--- PoshTools-VisualStudio2015/tools/ChocolateyInstall.ps1
+++ PoshTools-VisualStudio2015/tools/ChocolateyInstall.ps1
@@ -1,2 +1,2 @@
 ﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for Visual Studio VSIX deployment
-Install-ChocolateyVsixPackage -PackageName 'PoshTools-VisualStudio2015' -Url 'https://github.com/adamdriscoll/poshtools/releases/download/v3.0.158/PowerShellTools.14.0.vsix';
+Install-ChocolateyVsixPackage -PackageName 'PoshTools-VisualStudio2015' -Url 'https://github.com/adamdriscoll/poshtools/releases/download/v3.0.163/PowerShellTools.14.0.vsix';
