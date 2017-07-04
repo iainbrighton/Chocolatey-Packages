2ffcafeb6643249b00b67538a39c6b9989eff233
Iain Brighton <iainbrighton@hotmail.com>
2017-07-04 09:06:46 +0100

    Updates Nitro.Install to v5.5.9.2 Adds missing Chocolatey metadata

--- NitroReader.Install/tools/ChocolateyInstall.ps1
+++ NitroReader.Install/tools/ChocolateyInstall.ps1
@@ -1,9 +1,38 @@
-﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for extracted MSI installations
+$packageName = 'nitroreader.install'
+$checksum = 'EE1E777D7D46B09C5DCCB16EF91B5F181581156B575283251336DDB0EA0B569D';
+$checksum64 = '77720C4A6E1DDE7B0790A3375E0120821A2147307F62E0584B066F54E5200CE8';
+$msiChecksum = 'B34290DFC9558E3B639F5AD557DF3444212EB76D66941608068264AD8EC7F586';
+$msiChecksum64 = '2B29BB44A5E8A2F03F39E251EF00473E6C9A1A919E86CD3BA4CA330B14D5D3A0';
+
+## Template VirtualEngine.Build ChocolateyInstall.ps1 file for extracted MSI installations
 $packageToolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition;
 $packageDownloadPath = Join-Path -Path $packageToolsPath -ChildPath 'nitro_reader.exe';
-Get-ChocolateyWebFile -PackageName 'nitroreader.install' -FileFullPath $packageDownloadPath -Url 'http://install.nitropdf.com/reader/en/nitro_reader3.exe' -Url64bit 'http://install.nitropdf.com/reader/en/nitro_reader3_64.exe';
+$getChocolateyWebFileParams = @{
+    PackageName    = $packageName;
+    FileFullPath   = $packageDownloadPath;
+    Url            = 'http://install.nitropdf.com/reader5/en/nitro_reader5.exe';
+    Checksum       = $checksum;
+    ChecksumType   = 'sha256';
+    Url64bit       = 'http://install.nitropdf.com/reader5/en/nitro_reader5_64.exe';
+    Checksum64     = $checksum64;
+    ChecksumType64 = 'sha256';
+}
+Get-ChocolateyWebFile @getChocolateyWebFileParams;
+
 ## Extract Msi
 $packageExtractPath = Join-Path -Path $packageToolsPath -ChildPath 'nitro_reader.msi';
 Start-ChocolateyProcessAsAdmin -ExeToRun $packageDownloadPath -Statements "/EXTRACTMSI:$packageExtractPath" -Minimized;
+
 ## Install Msi
-Install-ChocolateyInstallPackage -PackageName 'nitroreader.install' -FileType 'MSI' -SilentArgs '/qn /norestart' -File $packageExtractPath -ValidExitCodes @(0,3010);
+$installChocolateyInstallPackageParams = @{
+    PackageName    = $packageName;
+    FileType       = 'MSI';
+    SilentArgs     = '/qn /norestart';
+    File           = $packageExtractPath;
+    ValidExitCodes = @(0,3010);
+    Checksum       = $msiChecksum;
+    ChecksumType   = 'sha256';
+    Checksum64     = $msiChecksum64;
+    ChecksumType64 = 'sha256';
+}
+Install-ChocolateyInstallPackage @installChocolateyInstallPackageParams;
--- NitroReader.Install/tools/ChocolateyUninstall.ps1
+++ NitroReader.Install/tools/ChocolateyUninstall.ps1
@@ -1,8 +1,16 @@
-﻿## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for MSI installations
+## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for MSI installations
 try {
-    Get-WmiObject -Class Win32_Product | Where { $_.Name -eq 'Nitro Reader 3' } | ForEach-Object {
-	    Uninstall-ChocolateyPackage -PackageName 'nitroreader.install' -FileType 'MSI' -SilentArgs "$($_.IdentifyingNumber) /qn /norestart" -ValidExitCodes @(0,3010); 
-    }
+    Get-CimInstance -Class Win32_Product |
+        Where { $_.Name -eq 'Nitro Reader 5' } |
+            ForEach-Object {
+                $uninstallChocolateyPackageParams = @{
+                    PackageName    = 'nitroreader.install';
+                    FileType       = 'MSI';
+                    SilentArgs     = "$($_.IdentifyingNumber) /qn /norestart"
+                    ValidExitCodes = @(0,3010); 
+                }
+                Uninstall-ChocolateyPackage @$uninstallChocolateyPackageParams;
+            }
 }
 catch {
     throw $_.Exception;
