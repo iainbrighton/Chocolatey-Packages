980043abe18e248ec0c91c37ec920cffd3d203a5
Iain Brighton <iainbrighton@hotmail.com>
2017-07-07 14:01:25 +0100

    Updates Citrix-Receiver to v4.8

--- Citrix-Receiver/tools/ChocolateyInstall.ps1
+++ Citrix-Receiver/tools/ChocolateyInstall.ps1
@@ -1,2 +1,11 @@
 ﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for EXE/MSI installations
-Install-ChocolateyPackage -PackageName 'Citrix-Receiver' -FileType 'EXE' -SilentArgs '/noreboot /silent' -Url 'http://downloadplugins.citrix.com/Windows/CitrixReceiver.exe' -ValidExitCodes @(0);
+$installChocolateyPackageParams = @{
+    PackageName    = 'Citrix-Receiver';
+    FileType       = 'EXE';
+    SilentArgs     = '/noreboot /silent';
+    Url            = 'http://downloadplugins.citrix.com/Windows/CitrixReceiver.exe';
+    ValidExitCodes = @(0,3010);
+    Checksum       = 'F7A05C9ECA4710667FE388715506E2B39DBE6A1EB7D11071CC00B5FDBBCF4AE4';
+    ChecksumType   = 'sha256';
+}
+Install-ChocolateyPackage @installChocolateyPackageParams;
