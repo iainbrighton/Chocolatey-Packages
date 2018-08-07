8ad48f9d7fc937047dba2d517709e7f867082be8
Iain Brighton <iainbrighton@hotmail.com>
2018-08-07 16:45:28 +0100

    Adds Citrix-Workspace application

---
+++ Citrix-Workspace/tools/ChocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for EXE/MSI installations
+$installChocolateyPackageParams = @{
+    PackageName    = 'Citrix-Workspace';
+    FileType       = 'EXE';
+    SilentArgs     = '/noreboot /silent';
+    Url            = 'https://downloadplugins.citrix.com/Windows/CitrixWorkspaceApp.exe';
+    ValidExitCodes = @(0,3010);
+    Checksum       = 'A189E74841E97D776520880C1D1CF5EEB52E536826D1AEA5E42C7163B2468140';
+    ChecksumType   = 'sha256';
+}
+Install-ChocolateyPackage @installChocolateyPackageParams;
---
+++ Citrix-Workspace/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,2 @@
+﻿## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for EXE installations
+Uninstall-ChocolateyPackage -PackageName 'Citrix-Workspace' -FileType 'EXE' -SilentArgs '/silent /uninstall /cleanup' -File "$env:ALLUSERSPROFILE\Citrix\Citrix Workspace 1808\TrolleyExpress.exe" -ValidExitCodes @(0,3010); 
