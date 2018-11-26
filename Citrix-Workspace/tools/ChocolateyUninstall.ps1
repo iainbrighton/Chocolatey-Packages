6a7734ba1a3463af54e8e6451534448bb922a2a1
kedf <20340291+kedf@users.noreply.github.com>
2018-11-26 15:58:39 +0100

    Update ChocolateyUninstall.ps1
    change directory for 1810

--- Citrix-Workspace/tools/ChocolateyUninstall.ps1
+++ Citrix-Workspace/tools/ChocolateyUninstall.ps1
@@ -1,2 +1,2 @@
 ﻿## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for EXE installations
-Uninstall-ChocolateyPackage -PackageName 'Citrix-Workspace' -FileType 'EXE' -SilentArgs '/silent /uninstall /cleanup' -File "$env:ALLUSERSPROFILE\Citrix\Citrix Workspace 1808\TrolleyExpress.exe" -ValidExitCodes @(0,3010); 
+Uninstall-ChocolateyPackage -PackageName 'Citrix-Workspace' -FileType 'EXE' -SilentArgs '/silent /uninstall /cleanup' -File "$env:ALLUSERSPROFILE\Citrix\Citrix Workspace 1810\TrolleyExpress.exe" -ValidExitCodes @(0,3010); 
