d8a09913932baef82c14c84a9e5145b2fedd3893
Sean Killeen <SeanKilleen@gmail.com>
2019-05-10 09:28:10 -0400

    Extract checksum into variable
    So that it's clear it needs to be updated manually and is an input.

--- Citrix-Workspace/tools/ChocolateyInstall.ps1
+++ Citrix-Workspace/tools/ChocolateyInstall.ps1
@@ -1,6 +1,7 @@
 ﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for EXE/MSI installations
 
 $citrixReleaseWebPage = "https://www.citrix.com/downloads/workspace-app/legacy-workspace-app-for-windows/workspace-app-for-windows-1903.html"
+$checksumFromReleasePage = "086DBE728BFBA427D1DF28C3A7ADEB072CD3878758084A547879759EE36064B5"
 
 $response = Invoke-WebRequest $citrixReleaseWebPage
 $relLink = $response.Links | Where-Object { $_.href -eq "#ctx-dl-eula" } | Select rel
@@ -12,7 +13,7 @@ $installChocolateyPackageParams = @{
     SilentArgs     = '/noreboot /silent';
     Url            = '$downloadLink';
     ValidExitCodes = @(0,3010);
-    Checksum       = '086DBE728BFBA427D1DF28C3A7ADEB072CD3878758084A547879759EE36064B5'; # Update manually
+    Checksum       = '$checksumFromReleasePage';
     ChecksumType   = 'sha256';
 }
 Install-ChocolateyPackage @installChocolateyPackageParams;
