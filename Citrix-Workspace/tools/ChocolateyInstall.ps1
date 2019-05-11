6b53e39dbb8ce39c3d4b472d6f707a7f37c6f004
Iain Brighton <iainbrighton@hotmail.com>
2019-05-11 09:07:01 +0100

    Updates Citrix-Workspace to 19.3.0.4

--- Citrix-Workspace/tools/ChocolateyInstall.ps1
+++ Citrix-Workspace/tools/ChocolateyInstall.ps1
@@ -1,19 +1,17 @@
-﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for EXE/MSI installations
+## Template VirtualEngine.Build ChocolateyInstall.ps1 file for EXE/MSI installations
 
-$citrixReleaseWebPage = "https://www.citrix.com/downloads/workspace-app/legacy-workspace-app-for-windows/workspace-app-for-windows-1903.html"
-$checksumFromReleasePage = "086DBE728BFBA427D1DF28C3A7ADEB072CD3878758084A547879759EE36064B5"
-
-$response = Invoke-WebRequest $citrixReleaseWebPage
-$relLink = $response.Links | Where-Object { $_.href -eq "#ctx-dl-eula" } | Select rel
-$downloadLink = "http:" + $relLink.rel
+$releaseUri = 'https://www.citrix.com/downloads/workspace-app/legacy-workspace-app-for-windows/workspace-app-for-windows-1903.html'
+$downloadLink = 'https:{0}' -f ((Invoke-WebRequest -Uri $releaseUri).Links | Where-Object { $_.href -eq '#ctx-dl-eula' } | Select-Object -ExpandProperty 'rel')
 
 $installChocolateyPackageParams = @{
-    PackageName    = 'Citrix-Workspace';
-    FileType       = 'EXE';
-    SilentArgs     = '/noreboot /silent';
-    Url            = '$downloadLink';
+    PackageName    = "Citrix-Workspace";
+    FileType       = "EXE";
+    SilentArgs     = "/noreboot /silent";
+    Url            = "$downloadUri";
     ValidExitCodes = @(0,3010);
-    Checksum       = '$checksumFromReleasePage';
-    ChecksumType   = 'sha256';
+    Checksum       = "086DBE728BFBA427D1DF28C3A7ADEB072CD3878758084A547879759EE36064B5";
+    ChecksumType   = "sha256";
 }
 Install-ChocolateyPackage @installChocolateyPackageParams;
+
+<#! POST-INSTALL-TASKS !#>
--- Citrix-Workspace/tools/ChocolateyUninstall.ps1
+++ Citrix-Workspace/tools/ChocolateyUninstall.ps1
@@ -1,2 +1,7 @@
 ﻿## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for EXE installations
-Uninstall-ChocolateyPackage -PackageName 'Citrix-Workspace' -FileType 'EXE' -SilentArgs '/silent /uninstall /cleanup' -File "$env:ALLUSERSPROFILE\Citrix\Citrix Workspace 1808\TrolleyExpress.exe" -ValidExitCodes @(0,3010); 
+
+<#! PRE-UNINSTALL-TASKS !#>
+
+Uninstall-ChocolateyPackage -PackageName 'Citrix-Workspace' -FileType 'EXE' -SilentArgs '/silent /uninstall /cleanup' -File "$env:ALLUSERSPROFILE\Citrix\Citrix Workspace 1903\TrolleyExpress.exe" -ValidExitCodes @(0,3010);
+
+<#! POST-UNINSTALL-TASKS !#>
