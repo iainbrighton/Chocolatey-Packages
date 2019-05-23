8b9ed32ccd6fd40f4864b90feeb690618091ce77
Iain <iainbrighton@hotmail.com>
2019-05-23 11:32:46 +0100

    Updates Citrix-Workspace to 1904.1

--- Citrix-Workspace/tools/ChocolateyInstall.ps1
+++ Citrix-Workspace/tools/ChocolateyInstall.ps1
@@ -1,12 +1,23 @@
 ## Template VirtualEngine.Build ChocolateyInstall.ps1 file for EXE/MSI installations
 
-$releaseUri = 'https://www.citrix.com/downloads/workspace-app/legacy-workspace-app-for-windows/workspace-app-for-windows-1903.html'
+$citrixWorkspaceAppVersion = '1904.1'
+<# Citrix change the download URL after a new version is released. Here we grab the latest download link from the RSS feed. #>
+Write-Host "Resolving Citrix Workspace app $citrixWorkspaceAppVersion download link.."
+$rssFeedUri = 'https://www.citrix.com/content/citrix/en_us/downloads/workspace-app.rss'
+$rssFeedWebResponse = (New-Object -TypeName System.Net.WebClient).DownloadString($rssFeedUri)
+$feed = [System.Xml.XmlDocument] $rssFeedWebResponse
+$releaseUri = $feed.rss.channel.item |
+                Where-Object { $_.Title -eq "New - Citrix Workspace app $citrixWorkspaceAppVersion for Windows" } |
+                    Select-Object -ExpandProperty link
+
 $htmlAgilityPackPath = '{0}\HtmlAgilityPack.dll' -f (Split-Path -Path $MyInvocation.MyCommand.Path)
 $null = [System.Reflection.Assembly]::LoadFrom($htmlAgilityPackPath)
-Write-Host 'Resolving download link..'
-$webResponse = (New-Object -TypeName System.Net.WebClient).DownloadString($releaseUri)
+
+<# Citrix sign the download link via Javascript so we have to parse the page to get the signed download Uri. #>
+Write-Host "Resolving Citrix Workspace app $citrixWorkspaceAppVersion download token.."
+$releaseUriWebResponse = (New-Object -TypeName System.Net.WebClient).DownloadString($releaseUri)
 $htmlDocument = New-Object -TypeName 'HtmlAgilityPack.HtmlDocument'
-$htmlDocument.LoadHtml($webResponse)
+$htmlDocument.LoadHtml($releaseUriWebResponse)
 $relativeUri = $htmlDocument.DocumentNode.SelectNodes('//a') |
     ForEach-Object { $_.Attributes } |
         Where-Object { $_.Value -match 'CitrixWorkspaceApp.exe' } |
@@ -19,7 +30,7 @@ $installChocolateyPackageParams = @{
     SilentArgs     = "/noreboot /silent";
     Url            = "$downloadUri";
     ValidExitCodes = @(0,3010);
-    Checksum       = "086DBE728BFBA427D1DF28C3A7ADEB072CD3878758084A547879759EE36064B5";
+    Checksum       = "1DA12FCFE95944693C9628C2CF3349102717317D3BFFDEDDF7384087383BA430";
     ChecksumType   = "sha256";
 }
 Install-ChocolateyPackage @installChocolateyPackageParams;
