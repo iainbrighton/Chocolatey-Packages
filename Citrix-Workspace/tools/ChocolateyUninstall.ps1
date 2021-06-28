5c02a868eb05520a0c81b1b53f5759b8a7ec61c4
Iain.Brighton <iain.brighton@virtualengine.co.uk>
2021-06-28 14:37:20 +0100

    Updates Citrix-Workspace to 21.6.0.47

--- Citrix-Workspace/tools/ChocolateyUninstall.ps1
+++ Citrix-Workspace/tools/ChocolateyUninstall.ps1
@@ -1,7 +1,7 @@
-## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for EXE installations
+﻿## Template VirtualEngine.Build ChocolateyUninstall.ps1 file for EXE installations
 
 <#! PRE-UNINSTALL-TASKS !#>
 
-Uninstall-ChocolateyPackage -PackageName 'Citrix-Workspace' -FileType 'EXE' -SilentArgs '/silent /uninstall /cleanup' -File "$((${env:ProgramFiles(x86)}, $env:ProgramFiles -ne $null)[0])\Citrix\Citrix Workspace 2105\TrolleyExpress.exe" -ValidExitCodes @(0,3010);
+Uninstall-ChocolateyPackage -PackageName 'Citrix-Workspace' -FileType 'EXE' -SilentArgs '/silent /uninstall /cleanup' -File "$((${env:ProgramFiles(x86)}, $env:ProgramFiles -ne $null)[0])\Citrix\Citrix Workspace 2106\TrolleyExpress.exe" -ValidExitCodes @(0,3010);
 
 <#! POST-UNINSTALL-TASKS !#>
