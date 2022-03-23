d54d468f853582453abedecfc2bc1d3422780aeb
Martin Nygaard Jensen <martin@ravager.dk>
2022-03-23 18:53:40 +0100

    Updated Citrix Workspace App LTSR to 2203 (new LTSR)

--- Citrix-Workspace-LTSR/tools/ChocolateyInstall.ps1
+++ Citrix-Workspace-LTSR/tools/ChocolateyInstall.ps1
@@ -6,9 +6,9 @@ $installChocolateyPackageParams = @{
     PackageName    = "Citrix-Workspace-LTSR";
     FileType       = "EXE";
     SilentArgs     = "/noreboot /silent /AutoUpdateCheck=disabled";
-    Url            = "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/Receiver/Win/CitrixWorkspaceApp19.12.6000.9.exe";
+    Url            = "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/Receiver/Win/CitrixWorkspaceApp22.3.0.38.exe";
     ValidExitCodes = @(0,3010,40008);
-    Checksum       = "c5abca7edb8076410151914e36f583f75bfca9d8f2b7798f84aa6ac4457f9b4f";
+    Checksum       = "bc61b5a29f05fb68f11c116c96d38ff76e6aaaa2612740b8bdf37620abfc757b";
     ChecksumType   = "sha256";
 }
 Install-ChocolateyPackage @installChocolateyPackageParams;
