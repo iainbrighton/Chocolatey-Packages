666791f97e8ba845882bef014320ce0a49847578
iain.brighton <iain.brighton@virtualengine.co.uk>
2022-08-09 13:09:15 +0100

    Corrects Citrix-Workspace 22.7.0.24 hash

--- Citrix-Workspace/tools/ChocolateyInstall.ps1
+++ Citrix-Workspace/tools/ChocolateyInstall.ps1
@@ -8,7 +8,7 @@ $installChocolateyPackageParams = @{
     SilentArgs     = "/noreboot /silent /AutoUpdateCheck=disabled";
     Url            = "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/Receiver/Win/CitrixWorkspaceApp22.7.0.24.exe";
     ValidExitCodes = @(0,3010,40008);
-    Checksum       = "97b1d25327596f736f008bece1f04fe271379bb8b0518758c8a9042e8cfe8f66c9";
+    Checksum       = "b1d25327596f736f008bece1f04fe271379bb8b0518758c8a9042e8cfe8f66c9";
     ChecksumType   = "sha256";
 }
 Install-ChocolateyPackage @installChocolateyPackageParams;
