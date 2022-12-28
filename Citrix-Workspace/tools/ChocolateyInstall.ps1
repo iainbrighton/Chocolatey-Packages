aedc8dfbdee6862969fd9dc07d10752c4b9b85fd
Martin Nygaard Jensen <martin@ravager.dk>
2022-12-28 14:08:46 +0100

    WorkspaceApp2212 with dynamic download link

--- Citrix-Workspace/tools/ChocolateyInstall.ps1
+++ Citrix-Workspace/tools/ChocolateyInstall.ps1
@@ -1,15 +1,30 @@
 ﻿## Template VirtualEngine.Build ChocolateyInstall.ps1 file for EXE/MSI installations
 
 <#! PRE-INSTALL-TASKS !#>
+#region version variables
+$version = "2212"
+$Checksum = "c132e32196e5d2ade424d3d37fc62bdd57d8c7f1e7db623c3df955fd9676e5d0";
+$ChecksumType = "sha256";
+#endregion version variables
+
+#Get download page for specific version
+$sourcePrime = "https://www.citrix.com/downloads/workspace-app/"
+$responsePrime = Invoke-WebRequest $sourcePrime
+$Link = ($responsePrime.Links | where outerHTML -like "*>Citrix Workspace app $version for Windows<*").href
+$SourceUrl = "https://www.citrix.com$Link"
+
+#Get downloadlink
+$response = Invoke-WebRequest $SourceUrl
+$downloadlink = "https:" + ($response.Links | where id -eq "downloadcomponent").rel
 
 $installChocolateyPackageParams = @{
     PackageName    = "Citrix-Workspace";
     FileType       = "EXE";
     SilentArgs     = "/noreboot /silent /AutoUpdateCheck=disabled";
-    Url            = "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/Receiver/Win/CitrixWorkspaceApp22.10.5.14.exe";
-    ValidExitCodes = @(0,3010,40008);
-    Checksum       = "aab4a3cabc70f7c682b360d08917da501a5f8dd405137761d7f569bb395e3b6f";
-    ChecksumType   = "sha256";
+    Url            = $downloadlink;
+    ValidExitCodes = @(0, 3010, 40008);
+    Checksum       = $Checksum;
+    ChecksumType   = $ChecksumType;
 }
 Install-ChocolateyPackage @installChocolateyPackageParams;
 
