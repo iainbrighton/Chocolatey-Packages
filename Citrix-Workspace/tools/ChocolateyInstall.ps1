93c922be93cab8bbc2169d001d60525091982ac3
Martin Nygaard Jensen <martin@ravager.dk>
2023-01-10 11:04:23 +0100

    Workspace App 2212

--- Citrix-Workspace/tools/ChocolateyInstall.ps1
+++ Citrix-Workspace/tools/ChocolateyInstall.ps1
@@ -6,9 +6,9 @@ $installChocolateyPackageParams = @{
     PackageName    = "Citrix-Workspace";
     FileType       = "EXE";
     SilentArgs     = "/noreboot /silent /AutoUpdateCheck=disabled";
-    Url            = "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/Receiver/Win/CitrixWorkspaceApp22.10.5.14.exe";
+    Url            = "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/Receiver/Win/CitrixWorkspaceApp22.12.0.48.exe";
     ValidExitCodes = @(0,3010,40008);
-    Checksum       = "aab4a3cabc70f7c682b360d08917da501a5f8dd405137761d7f569bb395e3b6f";
+    Checksum       = "c132e32196e5d2ade424d3d37fc62bdd57d8c7f1e7db623c3df955fd9676e5d0";
     ChecksumType   = "sha256";
 }
 Install-ChocolateyPackage @installChocolateyPackageParams;
