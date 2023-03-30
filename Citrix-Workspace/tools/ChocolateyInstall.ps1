cf922703f2eeab50495abab7882b766320b50b30
Martin Nygaard Jensen <martin@ravager.dk>
2023-03-30 10:24:11 +0000

    Workspace App 2303

--- Citrix-Workspace/tools/ChocolateyInstall.ps1
+++ Citrix-Workspace/tools/ChocolateyInstall.ps1
@@ -6,9 +6,9 @@ $installChocolateyPackageParams = @{
     PackageName    = "Citrix-Workspace";
     FileType       = "EXE";
     SilentArgs     = "/noreboot /silent /AutoUpdateCheck=disabled";
-    Url            = "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/Receiver/Win/CitrixWorkspaceApp23.2.0.38.exe";
+    Url            = "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/Receiver/Win/CitrixWorkspaceApp23.3.0.55.exe";
     ValidExitCodes = @(0,3010,40008);
-    Checksum       = "4fe75c3db3099e9ebc28743b27134932f4d7c64c3402c7314a1640ea94c8cce1";
+    Checksum       = "1391dcc4ea7463f98f960e582593e7f06679fd4d7423cdd92247c260e809f234";
     ChecksumType   = "sha256";
 }
 Install-ChocolateyPackage @installChocolateyPackageParams;
