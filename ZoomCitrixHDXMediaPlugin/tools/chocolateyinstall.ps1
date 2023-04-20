3807fb4c10f7834e6c370c90a723eaea4ea1e4e7
Martin Nygaard Jensen <martin@ravager.dk>
2023-04-20 13:24:54 +0000

    Zoom HDX plugin

---
+++ ZoomCitrixHDXMediaPlugin/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿
+$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://zoom.us/download/vdi/5.14.0.23370/ZoomVDIUniversalPlugin.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+
+  softwareName  = 'Zoom VDI Universal Plugin*'
+
+  checksum      = '28dc6cade57d0a0c1c7f8e15ecad9a8b56ef47f4fd05791088314b46873eb5fd'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" CUSTOMINSTALL=2 INSTALLFORCITRIX=1 DISABLEAUS=TRUE"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
