577be4f59ca8ada6af432c2bb59202532614ec09
Martin Nygaard Jensen <martin@ravager.dk>
2023-09-07 07:58:44 +0000

    Pre populating bitness

--- Zoom-VDI-Universal-Plugin/tools/chocolateyinstall.ps1
+++ Zoom-VDI-Universal-Plugin/tools/chocolateyinstall.ps1
@@ -8,13 +8,15 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'msi'
   url           = $url
-
+  url64         = $url64
   softwareName  = 'Zoom VDI Universal Plugin*'
 
   checksum      = '28dc6cade57d0a0c1c7f8e15ecad9a8b56ef47f4fd05791088314b46873eb5fd'
   checksumType  = 'sha256'
+  checksum64      = '28dc6cade57d0a0c1c7f8e15ecad9a8b56ef47f4fd05791088314b46873eb5fd'
+  checksumType64  = 'sha256'
 
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" CUSTOMINSTALL=2 INSTALLFORCITRIX=1 DISABLEAUS=TRUE"
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" DISABLEAUS=TRUE"
   validExitCodes= @(0, 3010, 1641)
 }
 
