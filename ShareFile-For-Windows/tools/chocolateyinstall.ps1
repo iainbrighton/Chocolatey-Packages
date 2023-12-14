252fbe9591a582b5f53cda1a97cb1e7705288d22
Martin Nygaard Jensen <martin@ravager.dk>
2023-12-14 08:20:02 +0000

    Sharefile 23.10.16.0

--- ShareFile-For-Windows/tools/chocolateyinstall.ps1
+++ ShareFile-For-Windows/tools/chocolateyinstall.ps1
@@ -1,27 +1,21 @@
-﻿$releaseUri = 'https://www.citrix.com/downloads/sharefile/clients-and-plug-ins/citrix-files-for-windows.html'
-$releaseExeName = '*ShareFileForWindows_x64_v23.7.12.0.msi*'
-write-host 'Resolving download link...'
-$links = (Invoke-WebRequest $releaseUri).links
-$relativeUri = $links | where {$_.rel -like $releaseExeName} | select-object -expandproperty rel
-if (!$relativeUri)
-{
-  throw "Download link not found. Package possibly outdated"
-}
-$downloadUri = 'https:' + $relativeUri
-write-host 'Obtained download link...'
+﻿$downloadUri64 = "https://dl.sharefile.com/cfwin/ShareFileForWindows_x64_v23.10.16.0.msi"
+$downloadUri = "https://dl.sharefile.com/cfwin/ShareFileForWindows_x86_v23.10.16.0.msi"
+$checksum64 = "8B0DDB4D47E5CD4F19A976526E7709D393698CB33B1EA29BCBE8E7E24DFA50F5"
+$checksum = "CDFA78FD3CB7629B02CBA96C46EF1FC24211AE2E71A3345F2F580BDB229B4758"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'msi'
-  url           = $downloadUri
-  softwareName  = 'ShareFile'
-
-  checksum      = '7ae48eeaba486fdb961b1c48a1d942897fbbc783397fa33976fc830d72c0a7a5'
-  checksumType  = 'sha256'
-
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-  validExitCodes= @(0, 3010, 1641)
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'msi'
+  url            = $downloadUri
+  Url64bit       = $downloadUri64
+  softwareName   = 'ShareFile'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  Checksum64     = $checksum64
+  ChecksumType64 = 'sha256'
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
