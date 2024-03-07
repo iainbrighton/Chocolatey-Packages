2c0dd49521e3bff7cc4a66e715ebc67c0ddc8eee
Martin Nygaard Jensen <martin@ravager.dk>
2024-03-07 12:53:59 +0000

    Citrix Enterprise Browser v122
    Signed-off-by: Martin Nygaard Jensen <martin@ravager.dk>

--- Citrix-Enterprise-Browser/tools/ChocolateyInstall.ps1
+++ Citrix-Enterprise-Browser/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ## Template VirtualEngine.Build ChocolateyInstall.ps1 file for EXE/MSI installations
 
 #region version variables
-$version = "120"
-$Checksum = "E118B29214D57CD6DD65F6854ADCDA1A8C905F824EE21B653D91F8F47666A45E";
+$version = "122"
+$Checksum = "69547A3A0816E0A55C151793576C13C816CCC5D611FD5610030FC47702179B41";
 $ChecksumType = "sha256";
 #endregion version variables
 
