e50ecc59de96d4e1096faf02a09e078e4d7bb1d4
Martin Nygaard Jensen <martin@ravager.dk>
2024-06-18 20:25:29 +0200

    ZoomVDIclient and -plugin 5.17.13
    Signed-off-by: Martin Nygaard Jensen <martin@ravager.dk>

--- Zoom-VDI-Universal-Plugin/tools/chocolateyinstall.ps1
+++ Zoom-VDI-Universal-Plugin/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://zoom.us/download/vdi/5.17.11.24850/ZoomVDIUniversalPlugin.msi'
-$url64      = "https://zoom.us/download/vdi/5.17.11.24850/ZoomVDIUniversalPluginx64.msi?archType=x64"
+$url        = 'https://zoom.us/download/vdi/5.17.13.25060/ZoomVDIUniversalPlugin.msi'
+$url64      = "https://zoom.us/download/vdi/5.17.13.25060/ZoomVDIUniversalPluginx64.msi?archType=x64"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,9 +12,9 @@ $packageArgs = @{
   url64         = $url64
   softwareName  = 'Zoom VDI Universal Plugin*'
 
-  checksum      = '0885486327AE9D65F4B4A0EB8C1B71160265A062455BD14858867E6CAD2FCB68'
+  checksum      = '65C690104D487926BC644B684FAB512A71349FAA0B7361CB77CF8028E5152D5C'
   checksumType  = 'sha256'
-  checksum64      = '20C53BEC11BCD738425683B21EE1903FA20F306592C084804BD80B42CBF758D7'
+  checksum64      = '813631C8DA3BAAE1458EFB410E8F1B8C4160F97029F8C4410CBFDE6F61B28BDA'
   checksumType64  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" DISABLEAUS=TRUE"
