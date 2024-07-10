77d973a7d7f43b379b4aa761a3ef82cb925f728c
iain.brighton <iain.brighton@virtualengine.co.uk>
2024-07-10 10:54:08 +0100

    Citrix Workspace App 2405

--- Citrix-Workspace/tools/ChocolateyInstall.ps1
+++ Citrix-Workspace/tools/ChocolateyInstall.ps1
@@ -32,9 +32,9 @@ $installChocolateyPackageParams = @{
     PackageName    = "Citrix-Workspace";
     FileType       = "EXE";
     SilentArgs     = "/noreboot /silent /AutoUpdateCheck=disabled";
-    Url            = "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/Receiver/Win/CitrixWorkspaceApp24.3.1.97.exe";
+    Url            = "https://downloadplugins.citrix.com/ReceiverUpdates/Prod/Receiver/Win/CitrixWorkspaceApp24.5.0.131.exe";
     ValidExitCodes = @(0,3010,40008);
-    Checksum       = "4c85eb887e6795dfa160446a23af76b1b80642302a083aad05312885c8aa6a5e";
+    Checksum       = "25a3792a0445cf865c306786071d8f4f3fb4c9e765860f897e20ccebf8379835";
     ChecksumType   = "sha256";
 }
 Install-ChocolateyPackage @installChocolateyPackageParams;
